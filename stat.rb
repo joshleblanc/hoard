b4eab98706e96a7be39a2777bde4084824e155f4
Joshua LeBlanc <jleblanc@hey.com>
2024-04-14 12:49:53 -0300

    feat: add basic stat class

---
+++ stat.rb
@@ -0,0 +1,24 @@
+module Hoard 
+    class Stat
+        attr :v, :max, :min
+
+        def initialize
+            init(0, 0, 0)
+        end
+
+        def init(value, max_or_min, max = nil)
+            if max == nil 
+                self.max = max_or_min
+                self.v = value
+            else 
+                self.max = max
+                self.min = max_or_min
+                self.v = value
+            end
+        end
+
+        def init_max_on_max(max)
+            init(max, 0, max)
+        end
+    end
+end

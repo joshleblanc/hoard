47378929a214f5327922698ec38579fba009e97a
Joshua LeBlanc <jleblanc@hey.com>
2024-04-16 20:31:42 -0300

    feat: add method to find script property

--- scriptable.rb
+++ scriptable.rb
@@ -15,5 +15,14 @@ module Hoard
                 script.send(met, *args) if script.respond_to?(met)
             end
         end
+
+        def find_script_property(name)
+            ivar = :"@#{name}"
+            scripts.each do |script|
+                next unless script.instance_variables.include?(ivar)
+                
+                return script.instance_variable_get(ivar)
+            end
+        end
     end
 end

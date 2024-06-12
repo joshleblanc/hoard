752ce577b47f51b46cf08a62cd1eda0343880f11
Joshua LeBlanc <jleblanc@hey.com>
2024-06-12 18:25:17 -0300

    feat: save data

--- scripts/pickup_script.rb
+++ scripts/pickup_script.rb
@@ -7,10 +7,28 @@ module Hoard
                 @quantity = quantity
             end
 
-            def on_collision(player)
-                player.send_to_scripts(:add_to_inventory, entity, @quantity)
+            def init 
+                save_data = entity.save_data_script.init
+
+                hide! if save_data.picked_up
+            end
+
+            def show!
+                entity.visible = true
+                entity.destroyed = false
+            end
+
+            def hide!
                 entity.visible = false
                 entity.destroyed = true
+            end
+
+            def on_collision(player)
+                player.send_to_scripts(:add_to_inventory, entity, @quantity)
+
+                hide! 
+
+                entity.send_to_scripts(:save, { picked_up: true })
             end
         end
     end

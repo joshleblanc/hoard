30e4d388131d7183a373a86bd58f41b750aa8bbc
Joshua LeBlanc <jleblanc@hey.com>
2024-06-12 19:18:22 -0300

    feat: generic serializer for scripts

--- script.rb
+++ script.rb
@@ -4,6 +4,22 @@ module Hoard
 
         attr_accessor :entity
 
+        def to_h 
+            {}.tap do |klass|
+                instance_variables.reject { _1 == :@entity || _1 == :@args }.each do |k|
+                    klass[k.to_s[1..-1].to_sym] = instance_variable_get(k)
+                end
+            end
+        end
+
+        def serialize 
+            to_h
+        end
+
+        def to_s 
+            serialize.to_s
+        end
+
         def update; end 
         def post_update; end 
         def pre_update; end 

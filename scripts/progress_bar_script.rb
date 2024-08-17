3026416fe68ff291e23a3c7e030fd8e5b844955c
Joshua LeBlanc <jleblanc@hey.com>
2024-08-17 09:30:34 -0300

    fix: render promps in ui space

--- scripts/progress_bar_script.rb
+++ scripts/progress_bar_script.rb
@@ -1,74 +1,74 @@
-module Hoard 
-    module Scripts 
-        class ProgressBarScript < Script
-            attr_reader :limit, :curr
+module Hoard
+  module Scripts
+    class ProgressBarScript < Script
+      attr_reader :limit, :curr
 
-            def initialize(limit: 100) 
-                @limit = limit
-                @curr = 0
-                @active = false
-                @done = false
-            end
+      def initialize(limit: 100)
+        @limit = limit
+        @curr = 0
+        @active = false
+        @done = false
+      end
 
-            def progress 
-                @curr / @limit   
-            end
+      def progress
+        @curr / @limit
+      end
 
-            def reset! 
-                @done = false 
-                @active = false
-            end
+      def reset!
+        @done = false
+        @active = false
+      end
 
-            def activate!
-                @done = false
-                @active = true
-            end
+      def activate!
+        @done = false
+        @active = true
+      end
 
-            def done?
-                progress >= 1 && @active
-            end
-            
-            def active?
-                @active
-            end
+      def done?
+        progress >= 1 && @active
+      end
 
-            def idle?
-                !@active 
-            end
-            
-            def update 
-                return unless @active 
+      def active?
+        @active
+      end
 
-                @curr += 1 unless done?
-            end
+      def idle?
+        !@active
+      end
 
-            def max_w 
-                entity.w * 2
-            end
+      def update
+        return unless @active
 
-            def post_update
-                return unless @active 
+        @curr += 1 unless done?
+      end
 
-                if done?
-                    outputs[:scene].labels << { 
-                        x: entity.x,
-                        y: entity.y.from_top + entity.h + 32,
-                        text: "Done!",
-                        size_px: 8,
-                        alignment_enum: 1,
-                        r: 255, g: 255, b: 255, a: 255
-                    }
-                else
-                    outputs[:scene].primitives << {
-                        x: entity.x - (max_w / 2),
-                        y: entity.y.from_top + entity.h + 20,
-                        w: max_w * progress,
-                        h: 4,
-                        r: 255, g: 255, b: 255,
-                        primitive_marker: :solid
-                    }
-                end
-            end
+      def max_w
+        entity.w * 2
+      end
+
+      def post_update
+        return unless @active
+
+        if done?
+          outputs[:ui].labels << {
+            x: Game.s.camera.level_to_global_x(entity.x),
+            y: entity.y - 64,
+            font_size: 1,
+            text: "Done!",
+            alignment_enum: 1,
+            r: 255, g: 255, b: 255, a: 255,
+          }
+        else
+          outputs[:scene].primitives << {
+            x: entity.x - (max_w / 2),
+            y: entity.y.from_top + entity.h + 20,
+            w: max_w * progress,
+            h: 4,
+            r: 255, g: 255, b: 255,
+            primitive_marker: :solid,
+          }
         end
+      end
     end
+  end
 end
--- scripts/prompt_script.rb
+++ scripts/prompt_script.rb
@@ -1,37 +1,38 @@
-module Hoard 
-    module Scripts 
-        class PromptScript < Script 
-            attr :prompt
+module Hoard
+  module Scripts
+    class PromptScript < Script
+      attr :prompt
 
-            def initialize(prompt: "")
-                @prompt = prompt 
-                @active = true
-            end
+      def initialize(prompt: "")
+        @prompt = prompt
+        @active = true
+      end
 
-            def disable!
-                @active = false 
-            end
+      def disable!
+        @active = false
+      end
 
-            def enable!
-                @active = true
-            end
+      def enable!
+        @active = true
+      end
 
-            def active?
-                @active
-            end
+      def active?
+        @active
+      end
 
-            def on_collision(player)
-                return unless active?
-                 
-                outputs[:scene].labels << { 
-                    x: entity.x,
-                    y: entity.y.from_top + entity.h + 32,
-                    text: prompt,
-                    size_px: 8,
-                    alignment_enum: 1,
-                    r: 255, g: 255, b: 255, a: 255
-                }
-            end
-        end
+      def on_collision(player)
+        return unless active?
+
+        puts entity.y
+        outputs[:ui].labels << {
+          x: Game.s.camera.level_to_global_x(entity.x),
+          y: (entity.y),
+          text: prompt,
+          font_size: 1,
+          alignment_enum: 1,
+          r: 255, g: 255, b: 255, a: 255,
+        }
+      end
     end
+  end
 end

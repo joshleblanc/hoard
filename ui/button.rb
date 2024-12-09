3481b02c7cfe03c7df6005128168ea6eb678510c
Joshua LeBlanc <jleblanc@hey.com>
2024-12-09 07:42:24 -0400

    fix: remove duplicate on click call

--- ui/button.rb
+++ ui/button.rb
@@ -3,9 +3,6 @@ module Hoard
         class Button < Element 
             def render 
                 if hovered?
-                    if $args.inputs.mouse.click
-                        on_click&.call
-                    end
                     $args.outputs[:ui].sprites << {
                         x: rx, y: ry, w: rw, h: rh,
                         r: 255, g: 255, b: 255, a: 125

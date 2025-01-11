35b3cf367fb3af2983f84f3d35ca419cc2ce2db9
Joshua LeBlanc <jleblanc@hey.com>
2025-01-11 09:32:50 -0400

    fix: underscore before numbers

--- utils.rb
+++ utils.rb
@@ -1,14 +1,14 @@
-module Hoard 
-    class Utils 
-        def self.underscore(camel_cased_word)
-            camel_cased_word.to_s.split("::").last.bytes.map.with_index do |byte, i|
-                if byte > 64 && byte < 97
-                    downcased = byte + 32
-                    i.zero? ? downcased.chr : "_#{downcased.chr}"
-                else
-                    byte.chr
-                end
-            end.join
+module Hoard
+  class Utils
+    def self.underscore(camel_cased_word)
+      camel_cased_word.to_s.split("::").last.bytes.map.with_index do |byte, i|
+        if byte >= 48 && byte < 97
+          downcased = byte > 64 ? byte + 32 : byte
+          i.zero? ? downcased.chr : "_#{downcased.chr}"
+        else
+          byte.chr
         end
+      end.join
     end
+  end
 end

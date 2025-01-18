06ef588579528e6dffa8ffc1bd3b5f3dd4e521b6
Joshua LeBlanc <jleblanc@hey.com>
2025-01-18 11:42:15 -0400

    chore: move some game-player stuff to scripts

---
+++ scripts/move_to_neighbour_script.rb
@@ -0,0 +1,50 @@
+module Hoard
+  module Scripts
+    class MoveToNeighbourScript < Script
+      def on_pre_step_x
+        e = entity
+        if e.xr > 0.8
+          e.xr = 0.8 if e.has_collision(e.cx + 1, e.cy)
+          move_to_neighbour(e.wcx + 1, e.wcy) if e.has_exit?(e.cx + 1, e.cy)
+        end
+
+        if e.xr < 0.2
+          e.xr = 0.2 if e.has_collision(e.cx - 1, e.cy)
+          move_to_neighbour(e.wcx - 1, e.wcy) if e.has_exit?(e.cx - 1, e.cy)
+        end
+      end
+
+      def on_pre_step_y
+        e = entity
+        if e.yr >= 1
+          move_to_neighbour(e.wcx, e.wcy + 1) if e.has_exit?(e.cx, e.cy + 1)
+        end
+
+        if e.yr < 0.2
+          move_to_neighbour(e.wcx, e.wcy - 1) if e.has_exit?(e.cx, e.cy - 1)
+        end
+
+        if e.yr < 0.2
+          if e.has_collision(e.cx, e.cy - 1)
+            e.yr = 0.2
+          end
+        end
+      end
+
+      #
+      # Move an entity to a new level, given their current world coords
+      def move_to_neighbour(wx, wy)
+        level = Game.s.current_level
+        neighbour = level.find_neighbour(wx, wy)
+
+        return unless neighbour
+
+        wcx = (neighbour.world_x / Hoard::Const::GRID)
+        wcy = (neighbour.world_y / Hoard::Const::GRID)
+
+        entity.set_pos_case(wx - wcx, wy - wcy)
+        Game.s.start_level(neighbour)
+      end
+    end
+  end
+end
---
+++ scripts/platformer_controls_script.rb
@@ -0,0 +1,38 @@
+module Hoard
+  module Scripts
+    class PlatformerControlsScript < Script
+      def initialize(move_speed: 1)
+        @move_speed = move_speed
+        @current_speed = 0
+      end
+
+      def pre_update
+        @current_speed = 0
+
+        if Game.s.inputs.keyboard.key_held.left && !entity.cd.has("controls_disabled")
+          @current_speed = -@move_speed
+          entity.dir = -1
+          entity.send_to_scripts(:play_animation, :walk) if entity.on_ground?
+        elsif Game.s.inputs.keyboard.key_held.right && !entity.cd.has("controls_disabled")
+          @current_speed = @move_speed
+          entity.dir = 1
+
+          entity.send_to_scripts(:play_animation, :walk) if entity.on_ground?
+        else
+          entity.send_to_scripts(:play_animation, :idle) if entity.on_ground? && !entity.cd.has("landing")
+        end
+
+        if Game.s.inputs.keyboard.key_held.up && !entity.cd.has("controls_disabled")
+          @current_speed = 0
+          if Game.s.current_level.layer("Collisions").int(cx, cy) == 2 # ladder
+            entity.v_base.dy = -0.2
+          end
+        end
+      end
+
+      def update
+        entity.v_base.dx = (entity.v_base.dx + @current_speed) * 0.085 if @current_speed != 0
+      end
+    end
+  end
+end

799d75ceb5ba546bb4a39c3248ffbfc769a9b0bf
Joshua LeBlanc <jleblanc@hey.com>
2025-07-13 09:49:48 -0300

    fix: game would crash on ctrl+r reload

--- ui/window.rb
+++ ui/window.rb
@@ -12,22 +12,34 @@ module Hoard
       end
 
       def x
+        offset_x = state[:offset_x] || 0
+        offset_x = 0 unless offset_x.is_a?(Numeric)
+        
         if @options[:x]
-          @options[:x] + state[:offset_x]
+          @options[:x] + offset_x
         else
-          super + state[:offset_x]
+          super + offset_x
         end
       end
 
       def y
+        offset_y = state[:offset_y] || 0
+        offset_y = 0 unless offset_y.is_a?(Numeric)
+        
         if @options[:y]
-          @options[:y] + state[:offset_y]
+          @options[:y] + offset_y
         else
-          super + state[:offset_y]
+          super + offset_y
         end
       end
 
       def drag
+        # Ensure numeric state values
+        state[:offset_x] = 0 unless state[:offset_x].is_a?(Numeric)
+        state[:offset_y] = 0 unless state[:offset_y].is_a?(Numeric)
+        state[:drag_x] = 0 unless state[:drag_x].is_a?(Numeric)
+        state[:drag_y] = 0 unless state[:drag_y].is_a?(Numeric)
+        
         if $args.inputs.mouse.button_left
           if $args.inputs.mouse.inside_rect?([rx, ry, rw, rh]) && !state[:dragging]
             state[:dragging] = true

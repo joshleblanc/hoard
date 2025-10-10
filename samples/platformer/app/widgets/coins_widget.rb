af61e2b7815d95213c389fbf792a424303506783
Joshua LeBlanc <jleblanc@hey.com>
2025-10-10 12:49:05 -0300

    chore: simplify health widget

--- samples/platformer/app/widgets/coins_widget.rb
+++ samples/platformer/app/widgets/coins_widget.rb
@@ -6,17 +6,12 @@ class CoinsWidget < Hoard::Widget
     end
 
     def render
-        window(key: :coins, x: 16, y: 32.from_top, w: 720, h: 64) do
-            row do 
-                col(span: 0.5) do 
-                    image(path: "samples/platformer/sprites/spritesheet-tiles-default.png", w: 64, h: 64, tile_w: 64, tile_h: 64, tile_x: 13 * 64, tile_y: 1 * 64)
-                end
-                col do 
-                    image(path: "samples/platformer/sprites/spritesheet-tiles-default.png", w: 64, h: 64, tile_w: 64, tile_h: 64, tile_x: 13 * 64, tile_y: 3 * 64, offset_x: 32)
-                    coins.to_s.each_char.with_index do |char, i|
-                        image(path: "samples/platformer/sprites/spritesheet-tiles-default.png", w: 64, h: 64, tile_w: 64, tile_h: 64, tile_x: 13 * 64, tile_y: (13 - char.to_i) * 64, offset_x: i * -42)
-                    end
-                end
+        window(key: :coins, x: 0, y: 32.from_top, w: 720, h: 64) do
+            image(path: "samples/platformer/sprites/spritesheet-tiles-default.png", w: 64, h: 64, tile_w: 64, tile_h: 64, tile_x: 13 * 64, tile_y: 1 * 64, offset_x: 32)
+
+            image(path: "samples/platformer/sprites/spritesheet-tiles-default.png", w: 64, h: 64, tile_w: 64, tile_h: 64, tile_x: 13 * 64, tile_y: 3 * 64, offset_x: 32)
+            coins.to_s.each_char.with_index do |char, i|
+                image(path: "samples/platformer/sprites/spritesheet-tiles-default.png", w: 64, h: 64, tile_w: 64, tile_h: 64, tile_x: 13 * 64, tile_y: (13 - char.to_i) * 64, offset_x: i * -42)
             end
         end
     end

971355a680d3e5494bffd12b9f1af7900d435b18
Joshua LeBlanc <jleblanc@hey.com>
2025-10-10 12:49:36 -0300

    chore: simplify health widget

--- samples/platformer/app/widgets/coins_widget.rb
+++ samples/platformer/app/widgets/coins_widget.rb
@@ -8,7 +8,6 @@ class CoinsWidget < Hoard::Widget
     def render
         window(key: :coins, x: 0, y: 32.from_top, w: 720, h: 64) do
             image(path: "samples/platformer/sprites/spritesheet-tiles-default.png", w: 64, h: 64, tile_w: 64, tile_h: 64, tile_x: 13 * 64, tile_y: 1 * 64, offset_x: 32)
-
             image(path: "samples/platformer/sprites/spritesheet-tiles-default.png", w: 64, h: 64, tile_w: 64, tile_h: 64, tile_x: 13 * 64, tile_y: 3 * 64, offset_x: 32)
             coins.to_s.each_char.with_index do |char, i|
                 image(path: "samples/platformer/sprites/spritesheet-tiles-default.png", w: 64, h: 64, tile_w: 64, tile_h: 64, tile_x: 13 * 64, tile_y: (13 - char.to_i) * 64, offset_x: i * -42)

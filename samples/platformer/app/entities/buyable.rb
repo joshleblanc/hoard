bfcde41900bdf83356c6cc5be66e750ceb610ebc
Joshua LeBlanc <jleblanc@hey.com>
2025-10-11 18:00:21 -0300

    feat: store sample

--- samples/platformer/app/entities/buyable.rb
+++ samples/platformer/app/entities/buyable.rb
@@ -1,6 +1,6 @@
 class Buyable < Hoard::Entity 
     script Hoard::Scripts::LdtkEntityScript.new
+    script Hoard::Scripts::LabelScript.new
     script BuyableScript.new
     script Hoard::Scripts::PromptScript.new(prompt: "Press E to buy!")
-    script Hoard::Scripts::LabelScript.new
 end
--- samples/platformer/app/scripts/buyable_script.rb
+++ samples/platformer/app/scripts/buyable_script.rb
@@ -1,18 +1,17 @@
 class BuyableScript < Hoard::Script 
-    attr :price 
-    attr :item
-
+    attr :price, :item
+    
     def init 
         entity.label_script.label = "#{price} coins"
     end
 
     def on_interact(player)
-        if player.user.coins_script.can_afford?(price)
-            player.user.send_to_scripts(:add_notification, "", "You bought #{item}")
+        if player.user.coins_script.can_afford?(price) 
             player.user.send_to_scripts(:remove_from_inventory, nil, price)
-            #player.inventory_script.add_to_inventory(item)
+            player.user.send_to_scripts(:add_notification, "", "Bought #{item}")
         else 
-            player.user.send_to_scripts(:add_notification, "", "You don't have enough coins")
+            player.user.send_to_scripts(:add_notification, "", "Not enough coins")
         end
     end
 end
+    
